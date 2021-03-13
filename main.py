21add7a7ccf2b5211ae25003d2b90517e2d2df31
Samruddhi Mhatre <35568225+SamruddhiMhatre@users.noreply.github.com>
2021-03-13 14:04:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import nltk
-
-from clean_tweet import textFiltered, textTokenized
 from nltk.corpus import twitter_samples
 from nltk.tokenize import TweetTokenizer
 import random
@@ -8,13 +6,11 @@ import re
 from nltk.stem.wordnet import WordNetLemmatizer
 import string
 import tweepy
-import csv
-import time
-import json
 import pandas as pd
-from nltk import FreqDist
-from nltk.tokenize import word_tokenize
+import numpy as np
 
+
+# loading data
 twitter_sample = twitter_samples.fileids()
 # print(twitter_sample)
 
@@ -22,25 +18,49 @@ twitter_sample = twitter_samples.fileids()
 positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 
+print(len(positive_tweets))
+print(len(negative_tweets))
+
+# removing emojis
+
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+
+
+positive_noemo = []
+negative_noemo = []
+
+for tweets in positive_tweets:
+    positive_noemo.append(emoji_pattern.sub(r'', tweets))
+
+for tweets in negative_tweets:
+    negative_noemo.append(emoji_pattern.sub(r'', tweets))
+
+
 # tokenization
 tweet_tokenizer = TweetTokenizer()
-positive_tokenized = [tweet_tokenizer.tokenize(tweet) for tweet in positive_tweets]
-negative_tokenized = [tweet_tokenizer.tokenize(tweet) for tweet in negative_tweets]
+positive_tokenized = [tweet_tokenizer.tokenize(tweet) for tweet in positive_noemo]
+negative_tokenized = [tweet_tokenizer.tokenize(tweet) for tweet in negative_noemo]
 print('positive tokens:', positive_tokenized[:10])
 
 
 # filtering tokens
-def filter_tokens(tokens, stopwords=()):
+def filter_tokens(tweet_tokens, stopwords=()):
     filtered_tokens = []
-    for token in tokens:
+    for token in tweet_tokens:
         token = token.lower()
         token = re.sub(
             r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''',
             ' ', token)
-        token = re.sub('(@[A-Za-z0-9_]+)', '', token)
+        token = re.sub(r'[^A-Za-z0-9 ]+', '', token)
 
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token)  # word same as token
+
+        tweet_lemming = WordNetLemmatizer()
+        token = tweet_lemming.lemmatize(token)  # word same as token
 
         if len(token) > 0 and token not in string.punctuation and token not in stopwords:
             filtered_tokens.append(token)
@@ -57,143 +77,242 @@ for tokens in positive_tokenized:
 for tokens in negative_tokenized:
     negative_filtered.append(filter_tokens(tokens))
 
-print('positive filtered: ',positive_filtered[:10])
-print('negative filtered: ',negative_filtered[:10])
-#
-# for fd in positive_filtered:
-#     fd = nltk.FreqDist([w for w in fd])
-# print(fd['top'])
+print('positive filtered: ', positive_filtered[:10])
+print('negative filtered: ', negative_filtered[:10])
 
+positive_data = [(tweet, 'Positive') for tweet in positive_filtered]
+negative_data = [(tweet, 'Negative') for tweet in negative_filtered]
+print('positve data: ', positive_data[:10])
 
-
-
-# data creation
-def model_data(filtered):
-    for tokens in filtered:
-        yield dict([token, True] for token in tokens)
-
-
-positive_model_dict = model_data(positive_filtered)
-negative_model_dict = model_data(negative_filtered)
-
-positive_data = [(tweet, 'Positive') for tweet in positive_model_dict]
-negative_data = [(tweet, 'Negative') for tweet in negative_model_dict]
-print('positve data: ',positive_data[:10])
 
 dataset = positive_data + negative_data
 random.shuffle(dataset)
 print(dataset[:20])
 
+dataframe = pd.DataFrame(dataset,columns=['tweet','label'])
+print(dataframe.head())
+df_copy = dataframe.copy()
 
-train_data = dataset[: round(len(dataset) * 0.7)]
-test_data = dataset[round(len(dataset) * 0.7):]
-
-from nltk import classify
-from nltk import NaiveBayesClassifier
-
-classifier = NaiveBayesClassifier.train(train_data)
-print("Training accuracy is:{}\n".format(classify.accuracy(classifier, train_data)))
-print("Testing accuracy is:{}\n".format(classify.accuracy(classifier, test_data)))
-print(classifier.show_most_informative_features(10))
-# print(ensemble_clf.classify(feats), ensemble_clf.confidence(feats))
-
-test_tweet = "If I could give less than one star, that would have been my choice.  I rent a home and Per my lease agreement it is MY responsibility to pay their Pool Service company.  Within the last year they changed to PoolServ.  I have had  major issues with new techs every week, never checking PH balances, cleaning the filter, and not showing up at all 2 weeks in the past 2 months. I have had 4 different techs in the past 4 weeks.   I have emailed and called them and they never respond back nor even acknowledged my concerns or requests.  I cannot change companies but I'm required to still pay for lousy or no service.  Attached are a couple pictures of my pool recently due to one tech just didn't put any chlorine in it at all according to the tech who came the following week to attempt to clean it up.  Please think twice before working with these people.  No one wants to work with a business that doesn't return phone calls or emails."
-test_token = tweet_tokenizer.tokenize(test_tweet)
-test_filtered = filter_tokens(test_token)
-
-from nltk.sentiment import SentimentIntensityAnalyzer
-nltk.download('vader_lexicon')
-sia = SentimentIntensityAnalyzer()
-print(sia.polarity_scores(test_tweet))
-print(sia.polarity_scores(test_tweet)['pos'])
-
-print(test_tweet)
-print(classifier.classify(dict([token, True] for token in test_filtered)))
+dataframe['label'].replace({"Negative": 0, "Positive": 1}, inplace=True)
 
 
-token = '1337701516203806722-Tc0qgs0oxB0oOqK1CMtXt4u3NmvqOu'
-token_secret = 'BYjtc1Yfi5WJkM8ODHNiH4pSZtfxB49C4ELzJ2pOMTlm0'
-consumer_key = 'EPvgyr0BY3sfJQex4qyKGMGUW'
-consumer_secret = '1XwRpfw2YZOat9gD9OLf8RC5qw0HqxUL0Dvp3QmQ33ffoJlTiD'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(token, token_secret)
-api = tweepy.API(auth)
+def try_join(token_list):
+    try:
+        return ','.join(map(str, token_list))
+    except TypeError:
+        return np.nan
 
 
-def get_tweets(keyword):  # keyword can be username or hash tag
+dataframe['tweet'] = [try_join(token_list) for token_list in dataframe['tweet']]
 
-    number_of_tweets = 100
-    tweets = api.search(q=keyword, count=number_of_tweets, lang='eng')
-
-    return tweets
-
-
-def get_user_tweets(username):  # gets tweets from users timeline
-    number_of_tweets = 100
-    tweets = api.user_timeline(screen_name=username, count=number_of_tweets)
-
-    return tweets
+train_df = dataframe[:7000]
+test_df = dataframe['tweet'][7000:]
+print(train_df.head())
+print(test_df.head())
 
 
-def create_test_data(tweets):  # preparing test data
-    return [{'tweet': tweet.text, 'lable': None} for tweet in tweets]
+# print(train_df.info())
+
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english',lowercase=False)
+bag_of_words = vectorizer.fit_transform(dataframe['tweet'])
+print(bag_of_words)
 
 
-# user_tweets = get_user_tweets('@Thom_astro')
-# tweets = get_tweets('#covid19')
-# print(tweets)
-query = 'covid19'
-max_tweets = 2000
-tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
-my_list_of_dicts = []
-for each_json_tweet in tweets:
-    my_list_of_dicts.append(each_json_tweet._json)
+train_bow = bag_of_words[:7000]
+test_bow = bag_of_words[7000:]
+# print('test df')
+print('bow')
+print(train_bow)
+print(test_bow)
 
-with open('tweet_json_Data.txt', 'w') as file:
-    file.write(json.dumps(my_list_of_dicts, indent=4))
 
-my_demo_list = []
-with open('tweet_json_Data.txt', encoding='utf-8') as json_file:
-    all_data = json.load(json_file)
-    for each_dictionary in all_data:
-        tweet_id = each_dictionary['id']
-        text = each_dictionary['text']
-        favorite_count = each_dictionary['favorite_count']
-        retweet_count = each_dictionary['retweet_count']
-        created_at = each_dictionary['created_at']
-        my_demo_list.append({'tweet_id': str(tweet_id),
-                             'text': str(text),
-                             'favorite_count': int(favorite_count),
-                             'retweet_count': int(retweet_count),
-                             'created_at': created_at,
-                             })
+# splitting data into training and validation set
+xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train_df['label'], random_state=42, test_size=0.3)
+#
+lreg = LogisticRegression()
+lreg.fit(xtrain_bow, ytrain) # training the model
+#
+prediction = lreg.predict_proba(xvalid_bow) # predicting on the validation set
+prediction_int = prediction[:,1] >= 0.5 # if prediction is greater than or equal to 0.3 than 1 else 0
+prediction_int = prediction_int.astype(np.int)
 
-        tweet_dataset = pd.DataFrame(my_demo_list, columns=
-        ['tweet_id', 'text',
-         'favorite_count', 'retweet_count',
-         'created_at'])
+print(f1_score(yvalid, prediction_int)) # calculating f1 score
+#
 
-# Writing tweet dataset ti csv file for future reference
+test_pred = lreg.predict_proba(test_bow)
+test_pred_int = test_pred[:,1] >= 0.5
+test_pred_int = test_pred_int.astype(np.int)
+test_df['label'] = test_pred_int
 
-sentiment = []
-sentiment_pos_score = []
-sentiment_neg_score = []
-for val in tweet_dataset['text']:
-    test_token = tweet_tokenizer.tokenize(val)
-    test_filtered = filter_tokens(test_token)
+y_pred = lreg.predict(xvalid_bow)
 
-    print(val)
-    print(classifier.classify(dict([token, True] for token in test_filtered)))
-    sentiment.append(classifier.classify(dict([token, True] for token in test_filtered)))
-    print(sia.polarity_scores(val))
-    sentiment_pos_score.append(sia.polarity_scores(val)['pos'])
-    sentiment_neg_score.append(sia.polarity_scores(val)['neg'])
-print(sentiment)
-tweet_dataset['sentiment'] = sentiment
-tweet_dataset['pos_score'] = sentiment_pos_score
-tweet_dataset['neg_score'] = sentiment_neg_score
+from sklearn import metrics
+print("Logistic Regression accuracy (in %):", metrics.accuracy_score(yvalid, y_pred) * 100)
+print(lreg.score(xvalid_bow,yvalid))
+print(lreg.score(xtrain_bow,ytrain))
 
-print(tweet_dataset[['text','sentiment','pos_score','neg_score']])
-tweet_dataset.to_csv('tweet_data.csv')
+# from sklearn.naive_bayes import GaussianNB
+#
+# gnb = GaussianNB()
+# gnb.fit(xtrain_bow, ytrain)
+#
+# y_pred = gnb.predict(xvalid_bow)
+#
+# from sklearn import metrics
+# print("Gaussian Naive Bayes accuracy(in %):", metrics.accuracy_score(yvalid, y_pred) * 100)
+
+# from sklearn import linear_model
+# lasso_reg = linear_model.Ridge(alpha=50,max_iter=100,tol=0.1)
+# lasso_reg.fit(xtrain_bow,ytrain)
+#
+# print(lasso_reg.score(xvalid_bow,yvalid))
+# print(lasso_reg.score(xtrain_bow,ytrain))
+
+
+import pickle
+from sklearn.pipeline import Pipeline
+xtrain_bow = train_df['tweet']
+ytrain = train_df['label']
+
+# build the pipeline
+pipe = Pipeline([('vect', CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english',lowercase=False)),
+                 ('model', LogisticRegression())])
+
+
+
+
+model = pipe.fit(xtrain_bow, ytrain)
+
+# export model
+with open('model.pkl', 'wb') as model_file:
+  pickle.dump(model, model_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# token = '1337701516203806722-Tc0qgs0oxB0oOqK1CMtXt4u3NmvqOu'
+# token_secret = 'BYjtc1Yfi5WJkM8ODHNiH4pSZtfxB49C4ELzJ2pOMTlm0'
+# consumer_key = 'EPvgyr0BY3sfJQex4qyKGMGUW'
+# consumer_secret = '1XwRpfw2YZOat9gD9OLf8RC5qw0HqxUL0Dvp3QmQ33ffoJlTiD'
+#
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(token, token_secret)
+# api = tweepy.API(auth)
+
+#
+# def get_tweets(keyword):  # keyword can be username or hash tag
+#
+#     number_of_tweets = 100
+#     twitter_tweets = api.search(q=keyword, count=number_of_tweets, lang='eng')
+#
+#     return twitter_tweets
+#
+#
+# def get_user_tweets(username):  # gets tweets from users timeline
+#     number_of_tweets = 100
+#     user_tweets = api.user_timeline(screen_name=username, count=number_of_tweets)
+#
+#     return user_tweets
+#
+#
+# def create_test_data(twitter_tweets):  # preparing test data
+#     return [{'tweet': tweet.text, 'label': None} for tweet in twitter_tweets]
+#
+#
+# # # user_tweets = get_user_tweets('@Thom_astro')
+#
+# tweets = get_tweets('#joy')
+# for t in tweets:
+#     print('\nNew Tweet\n',t)
